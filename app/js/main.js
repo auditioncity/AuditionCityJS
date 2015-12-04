a2f621e2e440eb20dda7b0989769adc0749d75bc
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-12-04 10:52:44 -0500

    Added number type to actor info

--- app/js/main.js
+++ app/js/main.js
@@ -40,7 +40,44 @@ var registerActorController = function registerActorController() {
 
   var vm = this;
   vm.title = 'Register as an actor';
-  console.log('hi');
+
+  vm.unions = ['Equity', 'AFTRA', 'SAG', 'IATSE', 'SSDC', 'DGA', 'AGMA'];
+
+  vm.selection = [];
+
+  vm.toggleSelection = function (union) {
+    var idx = vm.selection.indexOf(union);
+    if (idx > -1) {
+      vm.selection.splice(idx, 1);
+    } else {
+      vm.selection.push(union);
+    }
+  };
+
+  vm.actor = {
+    name: null,
+    address: null,
+    phone: null,
+    email: null,
+    Website: null,
+    bio: null,
+    age: null,
+    height_feet: null,
+    height_inches: null,
+    hair_color: null,
+    eye_color: null,
+    gender: null,
+    ethnicity: null,
+    talent_agency: null,
+    union: [],
+    skills: [],
+    headshot: null,
+    resume: null
+  };
+
+  vm.sendData = function (actor) {
+    console.log(actor);
+  };
 };
 
 registerActorController.$inject = [];
